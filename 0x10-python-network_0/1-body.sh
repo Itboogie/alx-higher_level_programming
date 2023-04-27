7582e5e72e7f911f14d64bc049f777eb6f40aff1
Workshop8321 <Udaknse111@gmail.com>
2023-04-27 15:34:49 -0700

    tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-# Take in URL, display body of a 200 response; Usage: ./1-body.sh 5B0.0.0.0:5000/route_1 ; echo ""
+# By: Imanol Asolo
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
