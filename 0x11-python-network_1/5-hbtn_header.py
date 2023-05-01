6bc95da3b0213f477817db2003c8f3690c9f4c21
Workshop8321 <Udaknse111@gmail.com>
2023-05-01 11:08:50 -0700

    tasks

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""
-fetch https://intranet.hbtn.io/status; display response
-"""
+"""Check status"""
 import requests
+import sys
+
+
+def header():
+    """status"""
+    result = requests.get(sys.argv[1])
+
+    print(result.headers.get("X-Request-Id", None))
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    header()
